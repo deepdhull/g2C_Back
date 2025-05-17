7478133814aae6c5a8eafa082a60e905331d33d8
deepdhull <140973972+deepdhull@users.noreply.github.com>
2025-05-17 07:52:11 +0530

    c1

---
+++ config/dbConfig.js
@@ -0,0 +1,3 @@
+// const dburl = "mongodb://localhost:27017/BCE2024";
+const dburl = "mongodb+srv://parnoork24:Admin123@g2c.g5ipgox.mongodb.net/BCE2024?retryWrites=true&w=majority&appName=G2C"
+module.exports={dburl};
